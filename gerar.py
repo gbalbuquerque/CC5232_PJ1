51805d22fb99aaf910f0a77de8aa80c9e744fb08
Guilherme Barboza de Albuquerque <gui67785@gmail.com>
2024-10-20 23:59:11 -0300

    Inserção de dados aleatórios na table artista e disco

--- gerar.py
+++ gerar.py
@@ -2,6 +2,9 @@ import mysql.connector
 from faker import Faker
 from lorem_text import lorem
 
+fake = Faker()
+
+#Conexão com o db
 conexao = mysql.connector.connect(
     host = 'localhost',
     database = 'spotify',
@@ -12,8 +15,22 @@ conexao = mysql.connector.connect(
 if conexao.is_connected():
     print("Conectado ao banco de dados com sucesso!")
     cursor = conexao.cursor()
+    
+# Dados aleatórios artista
+for _ in range(20):
+    nome = fake.name()
+    cursor.execute("INSERT INTO artista (nome) VALUES (%s)", (nome,))
+for _ in range(20):
+    data = fake.date()
+    novaData = data.replace('-','/')  
+    cursor.execute("INSERT INTO artista (data_nascimento) VALUES (%s)",(novaData))
 
+# Dados aleatórios disco
+for _ in range(20):
+    titulo = lorem.words(4)
+    cursor.execute("INSERT INTO disco (titulo) VALUES (%s)", (titulo))
 
+conexao.commit()
 
 conexao.close()
 cursor.close()    
